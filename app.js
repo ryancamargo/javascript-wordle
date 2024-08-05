e27aead5126f5f32b3821758c9aa14285c2f312e
Ryan Camargo <90472475+ryancamargo@users.noreply.github.com>
2024-08-05 11:27:29 -0300

    add event listener

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
+const letters = document.querySelectorAll('.scoreboard-letter');
+//console.log(letters);
+const loadindDiv = document.querySelector('.info-bar');
+
+async function init() {
+
+
+
+
+    document.addEventListener('keydown', function handleKeyPress (event) {
+        const action = event.key;
+
+        console.log(action);
+
+        if (action === 'Enter') {
+            commit();
+        } else if (action === 'Backspace') {
+            backspace();
+        } else if (isLetter(action)) {
+            addLetter(action.toUpperCase);
+        } else {
+            // do nothing
+        }
+    })
+}
 
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
+
+init();
